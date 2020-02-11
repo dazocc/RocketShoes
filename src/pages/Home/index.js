2a8bcac1ec2cafdd0a48e882bbc45ee5e55dc5a6
dazocc <dazocc@gmail.com>
2020-02-10 22:10:46 -0400

    fiftth commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
+import * as CartActions from '../../store/module/cart/actions';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -21,8 +24,14 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { addToCart } = this.props;
+    addToCart(product);
+  };
+
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
     return (
       <ProductList>
         {products.map(product => (
@@ -31,9 +40,13 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormat}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
-                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                <MdAddShoppingCart size={16} color="#FFF" />{' '}
+                {amount[product.id] || 0}
               </div>
               <span>ADICIONAR AO CARINHO</span>
             </button>
@@ -43,3 +56,14 @@ export default class Home extends Component {
     );
   }
 }
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
